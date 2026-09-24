9725ff68446a3d2411f10e42f43b97dadb1d0409
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2026-09-24 14:14:01 +0530

    update server log message to include full URL for local access

--- src/server.js
+++ src/server.js
@@ -149,5 +149,5 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://localhost${PORT}`);
 });
